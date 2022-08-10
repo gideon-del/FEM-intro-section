70f2f10a6d75772228d1d58496043e74db733d40
gideon-del <gideonchidi471@gmail.com>
2022-08-10 22:56:05 +0100

    Adding mobile view

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 "use strict";
 
 const first = document.querySelectorAll(".nav__list");
+const menu = document.querySelector(".nav__container");
+const overlay = document.querySelector(".overlay");
+const openBtn = document.querySelector(".header__toggle");
+const closeBtn = document.querySelector(".nav__close");
+console.log(menu, overlay);
 let dropdown;
 let srce;
 let img;
@@ -15,4 +20,12 @@ first.forEach((nav) => {
     img.src = img.dataset.src;
     img.dataset.src = srce;
   });
+});
+openBtn.addEventListener("click", function () {
+  menu.classList.add("menu");
+  overlay.classList.add("display");
+});
+closeBtn.addEventListener("click", function () {
+  menu.classList.remove("menu");
+  overlay.classList.remove("display");
 });
